e13e6e3af347206d0141c0f1b4b0f21733fb73b7
alyssonfp <alyssonfp@gmail.com>
2021-08-02 20:25:30 -0300

    projeto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import 'assets/styles/custom.scss';
 import './App.css';
+import Routes from './Routes';
 
 function App() {
   return (
-   <h1>Hello Word</h1>
+    <Routes />
   );
 }
 
---
+++ src/pages/GitSearch/index.tsx
@@ -0,0 +1,87 @@
+import "./styles.css";
+import ResultCard from "components/ResultCard";
+import { useState } from "react";
+import axios from "axios";
+import gitImage from './gitimage.png'
+
+type Data = {
+  url: string;
+  followers: string;
+  location: string;
+  name: string;
+  avatar_url: string;
+};
+type FormData = {
+  git: string;
+};
+
+const GitSearch = () => {
+  const [data, setData] = useState<Data>();
+
+  const [formData, setFormData] = useState<FormData>({
+    git: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    axios
+      .get(`https://api.github.com/users/${formData.git}`)
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setData(undefined);
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="cep-search-container">
+      <div className="container search-container">
+        <form onSubmit={handleSubmit}>
+          <div className="form-container">
+            <h1>Encontre um perfil Github</h1>
+            <input
+              type="text"
+              name="git"
+              value={formData.git}
+              className="search-input"
+              placeholder="Usuário Github"
+              onChange={handleChange}
+            />
+            <button type="submit" className="btn btn-primary search-button">
+              Encontrar
+            </button>
+          </div>
+        </form>
+      </div>
+      {data && (
+      <div className="cep-search-container-inf">
+        <div className="return-container">
+          
+         
+            <>
+          <h4>Informações</h4>
+          <ResultCard title="Perfil:" description={data.url} />
+          <ResultCard title="Seguidores:" description={data.followers} />           
+          <ResultCard title="Localidade:" description={data.location} />
+          <ResultCard title="Nome:" description={data.name} />
+          </>
+         
+        </div>
+        <img src={data?.avatar_url} alt="" />
+      </div>
+       )}
+    </div>
+  );
+};
+
+export default GitSearch;
